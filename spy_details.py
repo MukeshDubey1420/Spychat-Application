9dd1557452245c93c9d4bdcf5215297e292cd759
MukeshDubey <mukeshdubey1420@gmail.com>
2017-07-03 15:56:35 +0530

    spychat_project

---
+++ spy_details.py
@@ -0,0 +1,33 @@
+
+spy = {
+    'name': 'Mukesh Dubey',
+    'salutation': 'Mr.',
+    'age': 20,
+    'rating': 4.7,
+    'is_online': True
+
+}
+
+friends = [
+    {
+        'name': 'Rohit',
+        'saultation': 'Mr.',
+        'rating': 3.0,
+        'age': 22,
+        'chats': []
+    },
+    {
+        'name': 'Nitika Sharma',
+        'saultation': 'Ms.',
+        'rating': 4.95,
+        'age': 20,
+        'chats': []
+    },
+    {
+        'name': 'Mukul',
+        'saultation': 'Mr.',
+        'rating': 4.39,
+        'age': 37,
+        'chats': []
+    }
+]
